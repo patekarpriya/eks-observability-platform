255b743a72cbfc82ddcd3901e8dd974d13f9f6b5
priyapatekar <patekarpriya50@gmail.com>
2026-03-17 10:51:46 +0530

    frontend dockerfile app.js

---
+++ frontend/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({
+        message: "Backend API is running 🚀",
+        status: "success"
+    });
+});
+
+app.get('/health', (req, res) => {
+    res.send("OK");
+});
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
