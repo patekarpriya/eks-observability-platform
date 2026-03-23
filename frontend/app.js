f768e66cdb379cbbc8fe93ea3ebc03442236bfff
priyapatekar <patekarpriya50@gmail.com>
2026-03-23 12:37:02 +0530

    dockerfile

--- frontend/app.js
+++ frontend/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
+// Serve static files
+app.use(express.static(__dirname));
+
+// Show HTML page on "/"
 app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// API route (separate)
+app.get('/api', (req, res) => {
     res.json({
         message: "Backend API is running 🚀",
         status: "success"
